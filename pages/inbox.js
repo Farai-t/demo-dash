7f5350759c415d4049a0f792304126e7d90004e4
Farai <69221515+Farai-t@users.noreply.github.com>
2022-11-13 18:31:49 +0200

    Update inbox.js

--- pages/inbox.js
+++ pages/inbox.js
@@ -32,7 +32,7 @@ const Inbox = () => {
 </a>
 </div>
 <div class="pl-3">
-<a href="https://demos.creative-tim.com/soft-ui-flowbite-pro/mailing/compose/" class="text-white bg-gradient-to-br from-pink-500 to-voilet-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-3 shadow-md shadow-gray-300 hover:scale-[1.02] transition-transform">
+<a href="#" class="text-white bg-gradient-to-br from-pink-500 to-voilet-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-3 shadow-md shadow-gray-300 hover:scale-[1.02] transition-transform">
 <svg class="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
 </svg>
